f34f6e810fb96431aeafd1492c3732714c28267d
TidyMaze <rolland_yann@orange.fr>
2018-11-11 14:49:59 +0100

    resizing

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Graphics.Image.IO
 import Graphics.Image.ColorSpace
 import Graphics.Image.Types
+import Graphics.Image.Processing
 import Graphics.Image (dims)
 import Graphics.Image.Interface (BaseArray)
 import Data.String
@@ -10,32 +11,34 @@ import System.Directory
 
 type LocalizedImage = (FilePath, Image VS RGB Double)
 
+targetDimensions :: (Int, Int)
+targetDimensions = (1080, 1980)
+
 imagesPath :: FilePath
 imagesPath = "images"
 
 listImages :: IO[FilePath]
 listImages = fmap (map ((imagesPath ++ "/") ++)) (listDirectory imagesPath)
 
-logDims :: BaseArray arr cs e => Image arr cs e -> IO ()
-logDims = print . dims
-
 mkLocalizedImage :: FilePath -> Image VS RGB Double -> LocalizedImage
 mkLocalizedImage a b = (a, b)
 
 readImageAndDisplayIfPossible :: FilePath -> IO()
-readImageAndDisplayIfPossible p = fmap (\e -> (p, e)) maybeLoaded  >>= showImageOrLog
+readImageAndDisplayIfPossible p = fmap (\e -> (p, e)) maybeLoaded >>= showImageOrLog
   where
     maybeLoaded = readImage p :: IO (Either String (Image VS RGB Double))
 
 showImageOrLog :: (FilePath, Either String (Image VS RGB Double)) -> IO ()
 showImageOrLog (path, Left msg) = putStrLn ("FAILED to load file " ++ path ++ ": " ++ msg)
 showImageOrLog (path, Right img) = do
-  _ <- logDims img
-  displayImage img
+  _ <- putStrLn ("SUCCESS loading file " ++ path ++ " with dims " ++ show (dims img))
+  displayImage $ processImage img
 
 readAllImageAndDisplayIfPossible :: [FilePath] -> IO()
 readAllImageAndDisplayIfPossible = mapM_ readImageAndDisplayIfPossible
 
+processImage :: Image VS RGB Double -> Image VS RGB Double
+processImage = resize Bilinear Edge targetDimensions
 
 main :: IO ()
 main = listImages >>= readAllImageAndDisplayIfPossible
