8117b2dc5032dcab6c17fcced732cbd168ed8cad
TidyMaze <rolland_yann@orange.fr>
2018-11-11 14:27:22 +0100

    showing dims

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,12 @@ module Main where
 import Graphics.Image.IO
 import Graphics.Image.ColorSpace
 import Graphics.Image.Types
+import Graphics.Image (dims)
+import Graphics.Image.Interface (BaseArray)
 import Data.String
 import System.Directory
+
+type LocalizedImage = (FilePath, Image VS RGB Double)
 
 imagesPath :: FilePath
 imagesPath = "images"
@@ -12,15 +16,26 @@ imagesPath = "images"
 listImages :: IO[FilePath]
 listImages = fmap (map ((imagesPath ++ "/") ++)) (listDirectory imagesPath)
 
-readImageAndDisplayIfPossible :: FilePath -> IO()
-readImageAndDisplayIfPossible p = (readImage p :: IO (Either String (Image VS RGB Double))) >>= showImageOrLog
+logDims :: BaseArray arr cs e => Image arr cs e -> IO ()
+logDims = print . dims
 
-showImageOrLog :: Either String (Image VS RGB Double) -> IO ()
-showImageOrLog (Left msg) = putStrLn ("FAILED to load file: " ++ msg)
-showImageOrLog (Right img) = displayImage img
+mkLocalizedImage :: FilePath -> Image VS RGB Double -> LocalizedImage
+mkLocalizedImage a b = (a, b)
+
+readImageAndDisplayIfPossible :: FilePath -> IO()
+readImageAndDisplayIfPossible p = fmap (\e -> (p, e)) maybeLoaded  >>= showImageOrLog
+  where
+    maybeLoaded = readImage p :: IO (Either String (Image VS RGB Double))
+
+showImageOrLog :: (FilePath, Either String (Image VS RGB Double)) -> IO ()
+showImageOrLog (path, Left msg) = putStrLn ("FAILED to load file " ++ path ++ ": " ++ msg)
+showImageOrLog (path, Right img) = do
+  _ <- logDims img
+  displayImage img
 
 readAllImageAndDisplayIfPossible :: [FilePath] -> IO()
 readAllImageAndDisplayIfPossible = mapM_ readImageAndDisplayIfPossible
+
 
 main :: IO ()
 main = listImages >>= readAllImageAndDisplayIfPossible
