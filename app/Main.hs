c2913191dc052fba57c19d8328a0116a67757a17
TidyMaze <rolland_yann@orange.fr>
2018-11-11 20:07:17 +0100

    getting processed images

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Graphics.Image.Processing
 import Graphics.Image (dims)
 import Graphics.Image.Interface (BaseArray)
 import Data.String
+import Data.Maybe
 import System.Directory
 
 type LocalizedImage = (FilePath, Image VS RGB Double)
@@ -16,7 +17,11 @@ targetDimensions :: Dimensions
 targetDimensions = (600, 800)
 
 generateLayouts :: Int -> [Dimensions]
-generateLayouts nbImages = map (\width -> (ceiling (fromIntegral nbImages / fromIntegral width), width) ) [1 .. nbImages]
+generateLayouts nbImages = map layoutFromWidth [1 .. nbImages]
+  where
+    layoutFromWidth width = (height, width)
+      where
+        height = ceiling (fromIntegral nbImages / fromIntegral width)
 
 imagesPath :: FilePath
 imagesPath = "images"
@@ -24,24 +29,25 @@ imagesPath = "images"
 listImages :: IO[FilePath]
 listImages = fmap (map ((imagesPath ++ "/") ++)) (listDirectory imagesPath)
 
-mkLocalizedImage :: FilePath -> Image VS RGB Double -> LocalizedImage
-mkLocalizedImage a b = (a, b)
-
-readImageAndDisplayIfPossible :: FilePath -> IO()
-readImageAndDisplayIfPossible p = fmap (\e -> (p, e)) maybeLoaded >>= showImageOrLog
+readImageAndDisplayIfPossible :: FilePath -> IO (Maybe (Image VS RGB Double))
+readImageAndDisplayIfPossible p = fmap (\e -> (p, e)) maybeLoaded >>= handleImageLoadingResult
   where
     maybeLoaded = readImage p :: IO (Either String (Image VS RGB Double))
 
-showImageOrLog :: (FilePath, Either String (Image VS RGB Double)) -> IO ()
-showImageOrLog (path, Left msg) =
+handleImageLoadingResult :: (FilePath, Either String (Image VS RGB Double)) -> IO (Maybe (Image VS RGB Double))
+handleImageLoadingResult (path, Left msg) = do
   putStrLn ("FAILED to load file " ++ path ++ ": " ++ msg)
-showImageOrLog (path, Right img) =
+  return Nothing
+handleImageLoadingResult (path, Right img) =
   do
     _ <- putStrLn ("SUCCESS loading file " ++ path ++ " with dims " ++ show (dims img))
-    (displayImage . processImage) img
+    _ <- displayImage processedImage
+    return $ Just processedImage
+  where
+    processedImage = processImage img
 
-readAllImageAndDisplayIfPossible :: [FilePath] -> IO()
-readAllImageAndDisplayIfPossible = mapM_ readImageAndDisplayIfPossible
+collectAllImages :: [FilePath] -> IO[Image VS RGB Double]
+collectAllImages = fmap catMaybes . mapM readImageAndDisplayIfPossible
 
 resizeKeepRatioWithFill :: Dimensions -> Image VS RGB Double -> Image VS RGB Double
 resizeKeepRatioWithFill (tHeight, tWidth) img = resize Bilinear Edge destinationSize img
@@ -64,5 +70,8 @@ centeredCrop (tHeight, tWidth) img = crop (startY, startX) (tHeight,tWidth) img
 processImage :: Image VS RGB Double -> Image VS RGB Double
 processImage = centeredCrop targetDimensions . resizeKeepRatioWithFill targetDimensions
 
-main :: IO ()
-main = listImages >>= readAllImageAndDisplayIfPossible
+mergeImages :: [Image VS RGB Double] -> Image VS RGB Double
+mergeImages = head
+
+main :: IO (Image VS RGB Double)
+main = fmap mergeImages (listImages >>= collectAllImages)
